8a82bb8fa03aeeac12b1f04e26ade0d75aaac3d4
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-02-17 21:15:31 -0500

    Finalizacion de la logica del lado del servidor

--- server/TareasAPI.php
+++ server/TareasAPI.php
@@ -30,14 +30,14 @@ class TareasAPI
     function response($code = 200, $status = "", $message = "")
     {
         http_response_code($code);
-        if(!empty($status) && !empty($message)){
+        if(!empty($status) AND !empty($message)){
             $response = array("status" => $status,"message"=>$message);
             echo json_encode($response, JSON_PRETTY_PRINT);
         }
     }
     
     function processList(){
-        //se verifica la accion y se verifica que actue sobre la tabla tareas
+        //se verifica la accion y se    verifica que actue sobre la tabla tareas
         if($_GET['action'] == 'tareas'){
             // aquí se instancia un objeto de la clase tareasdb
             $tareasDB = new TareasDB();
@@ -55,6 +55,62 @@ class TareasAPI
         }else{
             $this->response(400);
         }
+    }
+    
+    function processSave()
+    {
+        // Se comprueba que trabaja en la tabla tareas
+        if($_GET['action']=='tareas'){
+            //Decodifica un string de JSON
+            $obj    = json_decode( file_get_contents('php://input') );
+            $objArr = (array)$obj;
+            
+            if(empty($objArr)){
+                $this->response(422,"error","Nothing to add. Check json");
+            }else if(isset($obj->titulo)){
+                $tareasDB = new TareasDB();
+                $tareasDB->create($obj->titulo, $obj->descripcion, $obj->prioridad );
+                $this->response(200,"success","new record added");
+            }else{
+                $this->response(422,"error","The property is not defined");
+            }
+        } else{
+            $this->response(400);
+        }
+    }
+    
+    function processUpdate()
+    {
+        if(isset($_GET['action']) AND isset($_GET['id'])){
+            if($_GET['action']=='tareas'){
+                $obj = json_decode( file_get_contents('php://input') );
+                $objArr = (array)$obj;
+                
+                if(empty($objArr)){
+                    $this->response(422,"error","Nothing to add. Check json");
+                }else if(isset($obj->titulo)){
+                    $tareasDB = new TareasDB();
+                    $tareasDB->update($_GET['id'], $obj->titulo, $obj->descripcion, $obj->prioridad );
+                    $this->response(200,"success","Record updated");
+                }else{
+                    $this->response(422,"error","The property is not defined");
+                }
+                exit;
+            }
+        }
+        $this->response(400);
+    }
+    
+    function processDelete(){
+        if(isset($_GET['action']) AND isset($_GET['id'])){
+            if($_GET['action']=='tareas'){
+                $tareasDB = new TareasDB();
+                $tareasDB->delete($_GET['id']);
+                $this->response(204);
+                exit;
+            }
+        }
+        $this->response(400);
     }
 }
 
